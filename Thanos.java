3aebd0cfb32a13b014a2fe89c63485f1dfaa76b1
Elea641 <eleavoliotis64@gmail.com>
2024-04-11 16:18:51 +0200

    quest collection 02

--- Thanos.java
+++ Thanos.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
@@ -12,13 +14,28 @@ public class Thanos {
         heroes.add(new Hero("Thor", 1501));
 
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
+        Comparator<Hero> comparatorByName = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero o1, Hero o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
 
+        Collections.sort(heroes, comparatorByName);
 
         System.out.println("\nOrder by name:");
         showList(heroes);
 
         // TODO 2: Add a Comparator and sort by age (descending)
 
+        Comparator<Hero> comparatorByAge = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero o1, Hero o2) {
+                return o2.getAge() - o1.getAge();
+            }
+        };
+
+        Collections.sort(heroes, comparatorByAge);
 
         System.out.println("\nOrder by age:");
         showList(heroes);
